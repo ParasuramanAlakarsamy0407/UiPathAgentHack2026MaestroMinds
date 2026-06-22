098d27305724e7fcab9132d32c26f69b34e48d9b
PARASURAMAN ALAKARSAMY <170913715+ParasuramanAlakarsamy0407@users.noreply.github.com>
2026-06-23 01:33:57 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,21 @@
 import os
 
-TEMPLATE_FILE = "templates/Carrier_Quote_Template.docx"
-OUTPUT_FOLDER = "generated"
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
 
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+TEMPLATE_FILE = os.path.join(
+    BASE_DIR,
+    "templates",
+    "Carrier_Quote_Template.docx"
+)
+
+OUTPUT_FOLDER = os.path.join(
+    BASE_DIR,
+    "generated"
+)
+
+os.makedirs(
+    OUTPUT_FOLDER,
+    exist_ok=True
+)
